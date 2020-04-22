7353fa42d3d7b39c35f4a19b28faaefd81413139
mgebra <mgebra@gmail.com>
2020-04-22 20:08:25 +0400

    added sleep and error service. added readme

---
+++ src/README.md
@@ -0,0 +1,6 @@
+# Spring Boot Rest Example
+
+#### Services:
+* [http://localhost:8082/api/echo?data=asd](hhttp://localhost:8082/api/echo?data=asd)
+* [http://localhost:8082/api/sleep?seconds=3](http://localhost:8082/api/sleep?seconds=3)
+* [http://localhost:8082/api/error?throwException=true](http://localhost:8082/api/error?throwException=true)
--- src/main/java/com/example/rest/controller/RestController.java
+++ src/main/java/com/example/rest/controller/RestController.java
@@ -5,6 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.concurrent.TimeUnit;
+
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
 public class RestController {
@@ -12,5 +14,21 @@ public class RestController {
     @GetMapping("echo")
     public ResponseEntity<String> echo(String data) {
         return new ResponseEntity<>(data, HttpStatus.OK);
+    }
+
+    @GetMapping("sleep")
+    public ResponseEntity<Integer> sleep(Integer seconds) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(seconds);
+
+        return new ResponseEntity<>(seconds, HttpStatus.OK);
+    }
+
+    @GetMapping("error")
+    public ResponseEntity<Boolean> error(boolean throwException) throws InterruptedException {
+        if (throwException) {
+            throw new RuntimeException();
+        }
+
+        return new ResponseEntity<>(throwException, HttpStatus.OK);
     }
 }
